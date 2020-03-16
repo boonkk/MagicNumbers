382952cd6f29dd450712b9c9733d4f44e630e923
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:27:31 +0100

    not throwing exception now - moved to SuffixProcessorInitializer

--- src/main/java/readers/SuffixReader.java
+++ src/main/java/readers/SuffixReader.java
@@ -6,10 +6,14 @@ import java.io.File;
 
 public abstract class SuffixReader {
     final File file;
-    public SuffixReader(File file) {
-        this.file = file;
+
+    SuffixReader(File file) {
+            this.file = file;
+    }
+
+    public File getFile() {
+        return this.file;
     }
 
     public abstract FileSuffix read() throws UnsupportedFileTypeException;
-
 }
