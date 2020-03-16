165cec1e7b38d5273784c2714127bedb8ed9a52d
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:42:45 +0100

    works for hardcoded paths

--- src/main/java/processor/SuffixProcessorInitializer.java
+++ src/main/java/processor/SuffixProcessorInitializer.java
@@ -4,7 +4,7 @@ import exception.UnsupportedFileTypeException;
 import readers.ByteSuffixReader;
 import readers.NameSuffixReader;
 import readers.TextFileReader;
-import utils.ConsoleHelper;
+import utils.OutputHandler;
 
 import java.io.File;
 
@@ -21,12 +21,11 @@ public class SuffixProcessorInitializer {
                 File file = new File(filePath);
 
                 if(!file.exists())
-                    ConsoleHelper.writeMessage(file.getName() + " cannot be found.");
+                    OutputHandler.writeMessage(file.getName() + " cannot be found.");
 
                 ByteSuffixReader byteSuffixReader = new ByteSuffixReader(file);
                 NameSuffixReader nameSuffixReader = new NameSuffixReader(file);
                 TextFileReader textFileReader = new TextFileReader(file);
-
                 try{
                     SuffixProcessor suffixProcessor = new SuffixProcessor(byteSuffixReader, nameSuffixReader, textFileReader);
                     suffixProcessor.processSuffixes();
