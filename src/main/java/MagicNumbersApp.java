a51a47dc96ff6d92d841c919edfcf5160d021d2b
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:46:10 +0100

    added method which append path to resources folder, still works for full paths

--- src/main/java/MagicNumbersApp.java
+++ src/main/java/MagicNumbersApp.java
@@ -7,4 +7,27 @@ public class MagicNumbersApp {
         SuffixProcessorInitializer suffixChecker = new SuffixProcessorInitializer(args);
         suffixChecker.execute();
     }
+
+    public static String[] appendPath(String[] files) {
+        String[] result = new String[files.length];
+
+        String slashOrBackSlash = File.separator;
+        StringBuilder pathToResourceFolder = new StringBuilder();
+        pathToResourceFolder.append(System.getProperty("user.dir"))
+                .append(File.separator)
+                .append("src")
+                .append(File.separator)
+                .append("main")
+                .append(File.separator)
+                .append("resources")
+                .append(File.separator);
+
+        String pathToResources = pathToResourceFolder.toString();
+
+        for(int i=0; i<files.length; i++) {
+            result[i] = pathToResources + files[i];
+        }
+
+        return result;
+    }
 }
