2494d68cec76acb49b658581b739fed10f727912
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:23:55 +0100

    refactored and changed names

--- src/main/java/utils/OutputHandler.java
+++ src/main/java/utils/OutputHandler.java
@@ -4,9 +4,8 @@ import readers.FileSuffix;
 
 import java.io.File;
 
-public final class ConsoleHelper {
-    //change method and fields names
-    private ConsoleHelper() {
+public final class OutputHandler {
+    private OutputHandler() {
         throw new AssertionError();
     }
 
@@ -14,11 +13,11 @@ public final class ConsoleHelper {
         System.out.println(message);
     }
 
-    public static void messageValid(File file, FileSuffix fileSuffix) {
+    public static void writeMessageValid(File file, FileSuffix fileSuffix) {
         writeMessage(String.format("%s: Extension %s is valid.",file.getName(), fileSuffix));
     }
 
-    public static void messageInvalid(File file, FileSuffix fileSuffixA, FileSuffix fileSuffixB ) {
+    public static void writeMessageInvalid(File file, FileSuffix fileSuffixA, FileSuffix fileSuffixB ) {
         writeMessage(String.format("%s: Extension is %s, while actually it's a %s.",file.getName(), fileSuffixA, fileSuffixB));
     }
 
