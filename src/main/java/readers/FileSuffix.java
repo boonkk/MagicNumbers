bd9e565f3290ea05f3ed63b39b703a4e51c1c827
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:28:45 +0100

    added RTF ACC HLP file types

--- src/main/java/readers/FileSuffix.java
+++ src/main/java/readers/FileSuffix.java
@@ -5,16 +5,15 @@ public enum FileSuffix {
     JPG(new String[]{"JPEG", "JPG", "JPE"}, new String[]{"FFD8"}, new String[]{"FFD9"},0),
     GIF(new String[]{"GIF"}, new String[]{"474946383761", "474946383961"}, new String[]{"003B"},0),
     TXT(new String[]{"TXT"}, new String[]{}, new String[]{}, 0),
-    PNG(new String[]{"PNG"}, new String[]{"89504E470D0A1A0A"}, new String[]{}, 0);
-    //todo add more types
-
-
+    RTF(new String[]{"RTF"}, new String[]{"7B5C727466"}, new String[]{"7D"}, 0),
+    AAC(new String[]{"AAC"}, new String[]{"FFF1", "FFF9"}, new String[]{}, 0),
+    PNG(new String[]{"PNG"}, new String[]{"89504E470D0A1A0A"}, new String[]{}, 0),
+    HLP(new String[]{"HLP"}, new String[]{"0000FFFFFFFF"}, new String[]{}, 6);
 
     private final String[] possibleExtensions;
     private final String[] possibleHexadecimalOpenings;
     private final String[] possibleHexadecimalEndings;
     private int offset;
-
 
     FileSuffix(String[] possibleExtensions, String[] possibleByteToHexOpenings, String[] possibleByteToHexEndings, int offset) {
 
