8a902b20c6c4639fb6d5530b2a9131e1c74af0ee
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:39:28 +0100

    works for fullpath in args

--- src/main/java/MagicNumbersApp.java
+++ src/main/java/MagicNumbersApp.java
@@ -4,18 +4,6 @@ import java.io.File;
 
 public class MagicNumbersApp {
     public static void main(String[] args) {
-        String slashOrBackSlash = File.separator;
-        StringBuilder pathToResourceFolder = new StringBuilder();
-        pathToResourceFolder.append(System.getProperty("user.dir"))
-                .append(File.separator)
-                .append("src")
-                .append(File.separator)
-                .append("main")
-                .append(File.separator)
-                .append("resources")
-                .append(File.separator);
-
-///
         SuffixProcessorInitializer suffixChecker = new SuffixProcessorInitializer(args);
         suffixChecker.execute();
     }
