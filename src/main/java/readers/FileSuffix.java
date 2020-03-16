10ac48117b5a00ecf995c3046b3f5b5178a8f5d7
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:02:50 +0100

    added PNG

--- src/main/java/readers/FileSuffix.java
+++ src/main/java/readers/FileSuffix.java
@@ -1,20 +1,11 @@
 package readers;
 
 public enum FileSuffix {
-    //describes file extension
-
-    //fields needed possible extensions array (i.e. jpg can be .jpeg, .jpg, .jpe etc.
-    //possible opening bytes (at start of file)
-    //possible ending bytes (some file types has also magic numbers at end of file (i.e gif)
-    //offset - some magic numbers are placed further in the file (i.e starts from 6 byte)
-
-
-    //a bit of hard coding
     //FILETYPE ([POSSIBLE EXTENSIONS][], [FIRST BYTES IN HEXADECIMAL][], [LAST BYTES IN HEXADECIMAL][], OFFSET)
     JPG(new String[]{"JPEG", "JPG", "JPE"}, new String[]{"FFD8"}, new String[]{"FFD9"},0),
     GIF(new String[]{"GIF"}, new String[]{"474946383761", "474946383961"}, new String[]{"003B"},0),
-    TXT(new String[]{"TXT"}, new String[]{}, new String[]{}, 0);
-
+    TXT(new String[]{"TXT"}, new String[]{}, new String[]{}, 0),
+    PNG(new String[]{"PNG"}, new String[]{"89504E470D0A1A0A"}, new String[]{}, 0);
     //todo add more types
 
 
