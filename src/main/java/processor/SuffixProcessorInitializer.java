523950a714c1ef702b701622675e0aad4f0291ca
marcinolech <mrc.olech@gmail.com>
2020-03-16 14:18:20 +0100

    deleted main method, refactored, throws exception if .jar started with no arguments

---
+++ src/main/java/processor/SuffixProcessorInitializer.java
@@ -0,0 +1,42 @@
+package processor;
+
+import exception.UnsupportedFileTypeException;
+import readers.ByteSuffixReader;
+import readers.NameSuffixReader;
+import readers.TextFileReader;
+import utils.ConsoleHelper;
+
+import java.io.File;
+
+public class SuffixProcessorInitializer {
+    private final String[] filePaths;
+
+    public SuffixProcessorInitializer(String[] filePaths) {
+        this.filePaths = filePaths;
+    }
+
+    public void execute() {
+        if( filePaths.length > 0 ) {
+            for (String filePath : filePaths) {
+                File file = new File(filePath);
+
+                if(!file.exists())
+                    ConsoleHelper.writeMessage(file.getName() + " cannot be found.");
+
+                ByteSuffixReader byteSuffixReader = new ByteSuffixReader(file);
+                NameSuffixReader nameSuffixReader = new NameSuffixReader(file);
+                TextFileReader textFileReader = new TextFileReader(file);
+
+                try{
+                    SuffixProcessor suffixProcessor = new SuffixProcessor(byteSuffixReader, nameSuffixReader, textFileReader);
+                    suffixProcessor.processSuffixes();
+                } catch (UnsupportedFileTypeException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Files not specified in arguments. Please start the app with valid arguments(file paths).");
+        }
+    }
+
+}
