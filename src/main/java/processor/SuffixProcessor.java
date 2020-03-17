2018a650aa4ec9eec1e2c51d92d23c14746ef8d4
marcinolech <mrc.olech@gmail.com>
2020-03-17 16:45:27 +0100

    supports names without extensions, recognizes empty txt now

--- src/main/java/processor/SuffixProcessor.java
+++ src/main/java/processor/SuffixProcessor.java
@@ -10,7 +10,7 @@ class SuffixProcessor {
     private final SuffixReader byteSuffixReader;
     private final SuffixReader nameSuffixReader;
     private final SuffixReader textFileReader;
-    private final FileSuffix fileSuffix;
+    private final FileSuffix nameSuffix;
     private final File file;
 
     SuffixProcessor(ByteSuffixReader byteSuffixReader,
@@ -20,29 +20,41 @@ class SuffixProcessor {
         this.textFileReader = textFileReader;
         this.byteSuffixReader = byteSuffixReader;
         this.nameSuffixReader = nameSuffixReader;
-        this.fileSuffix = getNameSuffix();
+        this.nameSuffix = getNameSuffix();
         this.file = nameSuffixReader.getFile();
     }
 
     void processSuffixes() throws UnsupportedFileTypeException {
         FileSuffix byteSuffix = byteSuffixReader.read();
-        if( fileSuffix.toString().equalsIgnoreCase("TXT") )
+        if( nameSuffix == null){
+            if(processTextFile())
+                OutputHandler.writeMessageValid(file,FileSuffix.TXT);
+            else if(byteSuffix == null)
+                throw new UnsupportedFileTypeException();
+            else
+                OutputHandler.writeMessageValid(file,byteSuffix);
+
+            return;
+        }
+        if( nameSuffix.toString().equalsIgnoreCase("TXT") )
             if( processTextFile() ) {
-                OutputHandler.writeMessageValid(file, fileSuffix);
+                OutputHandler.writeMessageValid(file, nameSuffix);
                 return;
             } else {
-                OutputHandler.writeMessageInvalid(file, fileSuffix, byteSuffix);
+                OutputHandler.writeMessageInvalid(file, nameSuffix, byteSuffix);
                 return;
             }
 
-        if( fileSuffix == byteSuffix )
-            OutputHandler.writeMessageValid(file, fileSuffix);
+
+
+        if( nameSuffix == byteSuffix )
+            OutputHandler.writeMessageValid(file, nameSuffix);
         else if( byteSuffix != null )
-            OutputHandler.writeMessageInvalid(file, fileSuffix, byteSuffix);
+            OutputHandler.writeMessageInvalid(file, nameSuffix, byteSuffix);
         else if( processTextFile() )
-            OutputHandler.writeMessageInvalid(file, fileSuffix, FileSuffix.TXT);
+            OutputHandler.writeMessageInvalid(file, nameSuffix, FileSuffix.TXT);
         else
-            throw new UnsupportedFileTypeException(String.format("Extension is %s, while actually it's not supported.", fileSuffix));
+            throw new UnsupportedFileTypeException(String.format("Extension is %s, while actually it's not supported.", nameSuffix));
 
     }
 
